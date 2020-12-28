1754bbe577174b6ee6cf2d0256c4f2c7febd64fe
EugeneFi1 <51033432+EugeneFi1@users.noreply.github.com>
2020-12-29 00:13:22 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ViewModulesComponent} from './view-modules/view-modules.component';
+import {EditModulesComponent} from './edit-modules/edit-modules.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'view', component: ViewModulesComponent},
+  {path: 'edit', component: EditModulesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
