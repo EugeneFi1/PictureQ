e6eb023fa96f4840082776f04c131d62cbadd4c8
EugeneFi1 <51033432+EugeneFi1@users.noreply.github.com>
2020-12-29 00:22:32 +0200

    insert picture

---
+++ src/app/edit-modules/insert-picture/insert-picture.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-insert-picture',
+  templateUrl: './insert-picture.component.html',
+  styleUrls: ['./insert-picture.component.css']
+})
+export class InsertPictureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
