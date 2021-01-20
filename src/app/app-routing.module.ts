af5ea94602a7a1f6806f44d6a7c841284a680af0
EugeneFi1 <51033432+EugeneFi1@users.noreply.github.com>
2021-01-20 11:43:34 +0200

    refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ViewModulesComponent} from './view-modules/view-modules.component';
 import {EditModulesComponent} from './edit-modules/edit-modules.component';
-import {InsertPictureComponent} from './edit-modules/insert-picture/insert-picture.component';
 
 const routes: Routes = [
   {path: 'view', component: ViewModulesComponent},
-  {path: 'edit', component: EditModulesComponent, children: [
-      {path: 'insert-picture', component: InsertPictureComponent}
-    ]}
+  {path: 'edit', component: EditModulesComponent}
 ];
 
 @NgModule({
