1a22e0f870e2c43b49ce79a873c20fe664c2a6f8
EugeneFi1 <51033432+EugeneFi1@users.noreply.github.com>
2021-07-15 00:18:20 +0300

    refactor

--- src/app/modules/edit-modules/edit-routing.module.ts
+++ src/app/modules/edit-modules/edit-routing.module.ts
@@ -4,7 +4,7 @@ import {NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 const routes: Routes = [
-  { path: '', component: CreateQuestComponent}
+  { path: 'create', component: CreateQuestComponent}
 ];
 
 @NgModule({
