8387b64a3bfae4e041932f1d442d3ff728679b46
Archi-Od <51030941+Archi-Od@users.noreply.github.com>
2021-07-16 20:16:12 +0300

    crud quest-service

---
+++ src/app/components/cover-loading/cover-loading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cover-loading',
+  templateUrl: './cover-loading.component.html',
+  styleUrls: ['./cover-loading.component.less']
+})
+export class CoverLoadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
